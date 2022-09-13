7577911d8d76da0101131a79ec57d66622daae04
nurovic <mertnurovic@gmail.com>
2022-09-14 02:09:58 +0300

    Updated Router

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../Pages/Home";
+import Movies from "../Pages/Movies";
 
 function index() {
 
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/movies" element={<Movies />} />
     </Routes>
   );
 }
